4ddfc24777a702260b8ab10a4ec15086d41bad30
abdelrahman-ibrahem <abdo.ibrahem1122@gmail.com>
2023-03-12 11:45:55 +0200

    Add Pagination feature

--- config/settings.py
+++ config/settings.py
@@ -61,10 +61,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 REST_FRAMEWORK = {
     # SETUP REST FRAMEWORK AUTHENTICATION
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ]
-
+    ],
+    # SETUP PAGINATION
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,
 }
 
 MIDDLEWARE = [
--- tasks/views.py
+++ tasks/views.py
@@ -2,12 +2,19 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from .seriailzers import  TaskSerializers
 from .models import Task
+from rest_framework.pagination import PageNumberPagination
 
+# add pagination
+class LargeResultsSetPagination(PageNumberPagination):
+    page_size = 1000
+    page_size_query_param = 'page_size'
+    max_page_size = 10000
 
 
 class ListCreateTask(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticated]
     queryset = Task.objects.all()
+    pagination_class = LargeResultsSetPagination
     serializer_class = TaskSerializers
 
 
